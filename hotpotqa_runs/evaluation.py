7294fcc26e7aaae9071d260ded6011cce30c3001
DEVdaVinci <BrownDevinM@gmail.com>
2025-01-29 13:13:30 -0600

    Added evaluation functionality: generate image generate similarity score     (i.e. compare orginal and generated images)

---
+++ hotpotqa_runs/evaluation.py
@@ -0,0 +1,38 @@
+from diffusers import DiffusionPipeline
+
+#similarity metric
+import torch
+import clip
+from PIL import Image
+import torch.nn as nn
+
+class StableDiffusionEval_test:
+    def __init__(self):
+        #self.modelType = modelType
+        self.pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
+        self.pipeline.to("cuda")
+        self.device = "cuda" if torch.cuda.is_available() else "cpu"
+        self.model, self.preprocess = clip.load("ViT-B/32", device=self.device)
+    def evaluatePrompt(self, prompt, originalImage):
+        generatedImage = self.generateImage(prompt)
+        similarityScore = self.evaluateGeneratedImage(image_og = originalImage, image_generated = generatedImage)
+        return similarityScore
+    def generateImage(self, prompt: str):
+        generatedImage = self.pipeline(prompt).images[0]
+        return generatedImage
+    def evaluateGeneratedImage(self, image_og, image_generated):
+        cos = torch.nn.CosineSimilarity(dim=0)
+
+        image_og_preprocess = self.preprocess(Image.open(image_og)).unsqueeze(0).to(self.device)
+        image_og_features = self.model.encode_image( image_og_preprocess)
+
+        image_generated_preprocess = self.preprocess(Image.open(image_generated)).unsqueeze(0).to(self.device)
+        image_generated_features = self.model.encode_image( image_generated_preprocess)
+
+        similarity = cos(image_og_features[0],image_generated_features[0]).item()
+        similarity = (similarity+1)/2
+        '''
+            if doPrint:
+                print("Image similarity", similarity)
+        '''
+        return similarity
