b8ed7ca7c38c4d5c12d4241b108d3f1242c9a412
DEVdaVinci <BrownDevinM@gmail.com>
2025-01-30 12:06:04 -0600

    Corrected evaluateGeneratedImage() function     The preprocess function was treating the actual image as the title of the image.     I just deleted the function Image.open() and passed the image to the preprocess() function directly I also added 2 lines to print the Transformers and Bitsandbytes package version.

--- hotpotqa_runs/evaluation.py
+++ hotpotqa_runs/evaluation.py
@@ -11,6 +11,7 @@ class StableDiffusionEval_test:
         #self.modelType = modelType
         self.pipeline = DiffusionPipeline.from_pretrained("runwayml/stable-diffusion-v1-5")
         self.pipeline.to("cuda")
+        #This is necessary for the similarity metric
         self.device = "cuda" if torch.cuda.is_available() else "cpu"
         self.model, self.preprocess = clip.load("ViT-B/32", device=self.device)
     def evaluatePrompt(self, prompt, originalImage):
@@ -23,10 +24,10 @@ class StableDiffusionEval_test:
     def evaluateGeneratedImage(self, image_og, image_generated):
         cos = torch.nn.CosineSimilarity(dim=0)
 
-        image_og_preprocess = self.preprocess(Image.open(image_og)).unsqueeze(0).to(self.device)
+        image_og_preprocess = self.preprocess(image_og).unsqueeze(0).to(self.device)
         image_og_features = self.model.encode_image( image_og_preprocess)
 
-        image_generated_preprocess = self.preprocess(Image.open(image_generated)).unsqueeze(0).to(self.device)
+        image_generated_preprocess = self.preprocess(image_generated).unsqueeze(0).to(self.device)
         image_generated_features = self.model.encode_image( image_generated_preprocess)
 
         similarity = cos(image_og_features[0],image_generated_features[0]).item()
