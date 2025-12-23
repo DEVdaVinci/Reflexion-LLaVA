ffe181716af172ff927c5945c488f7ac91f1d8fc
DEVdaVinci <123280364+DEVdaVinci@users.noreply.github.com>
2025-12-22 20:17:57 -0600

    moved print statement for prompt to model so it can be displayed multiple times / once for each call to the LLM

--- hotpotqa_runs/agents.py
+++ hotpotqa_runs/agents.py
@@ -457,7 +457,7 @@ class ActionLLM:
             inImage = inImages[1]
         
         
-        print(f"|------------------------------------------------------->\n|\t=======      \tPrompt to {self.modelType}\t=========\t>\n|------------------------------------------------------->\n|>{inPrompt}<|\n<---------------------------------------------------------------|\n<\t=======      \tPrompt to {self.modelType}\t=========\t|\n<---------------------------------------------------------------|")
+        #print(f"|------------------------------------------------------->\n|\t=======      \tPrompt to {self.modelType}\t=========\t>\n|------------------------------------------------------->\n|>{inPrompt}<|\n<---------------------------------------------------------------|\n<\t=======      \tPrompt to {self.modelType}\t=========\t|\n<---------------------------------------------------------------|")
         
         
         if(self.modelType == "LLaVA"):
@@ -480,6 +480,7 @@ class ActionLLM:
         numAttempts = 0
         #If a call to the llm fails retry until you suceed or reach maximum number attempts
         while extractedText == None and numAttempts < maxAttempts:
+            print(f"|------------------------------------------------------->\n|\t=======      \tPrompt to {self.modelType}\t=========\t>\n|------------------------------------------------------->\n|>{prompt}<|\n<---------------------------------------------------------------|\n<\t=======      \tPrompt to {self.modelType}\t=========\t|\n<---------------------------------------------------------------|")
             if image != None:
                 response = self.model(image, prompt=prompt, generate_kwargs={"max_new_tokens": max_new_tokens})
             else:
@@ -533,6 +534,7 @@ class ActionLLM:
         numAttempts = 0
         #If a call to the llm fails retry until you suceed or reach maximum number attempts
         while extractedText == None and numAttempts < maxAttempts:
+            print(f"|------------------------------------------------------->\n|\t=======      \tPrompt to {self.modelType}\t=========\t>\n|------------------------------------------------------->\n|>{prompt}<|\n<---------------------------------------------------------------|\n<\t=======      \tPrompt to {self.modelType}\t=========\t|\n<---------------------------------------------------------------|")
             response = client.chat.completions.create(
                 model="gpt-4o",
                 messages=[
@@ -596,6 +598,7 @@ class ActionLLM:
         numAttempts = 0
         #If a call to the llm fails retry until you suceed or reach maximum number attempts
         while extractedText == None and numAttempts < maxAttempts:
+            print(f"|------------------------------------------------------->\n|\t=======      \tPrompt to {self.modelType}\t=========\t>\n|------------------------------------------------------->\n|>{prompt}<|\n<---------------------------------------------------------------|\n<\t=======      \tPrompt to {self.modelType}\t=========\t|\n<---------------------------------------------------------------|")
             response = client.chat.completions.create(
                 model=self.modelType,
                 messages=[
